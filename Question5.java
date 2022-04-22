8d96f6fe4cfc9e8615b0570e988feb8a555b523e
AndySimHH <92966185+AndySimHH@users.noreply.github.com>
2022-04-23 01:26:01 +0800

    Update Question5.java

--- Question5.java
+++ Question5.java
@@ -1,74 +1,61 @@
 import java.util.Scanner;
-
-public class Question5
+class Question5
 {
-  public static void main(String[] args)
-  {
-    /**
-     * Prompt the user for number. This input indicates the number of integers the user will be entering next. 
-     * Print out the mode (highest occurrence) from the set of integers. 
-     *    e.g.
-     *     > 5
-     *     > 2
-     *     > 4
-     *     > 1
-     *     > 3
-     *     > 4
-     *     4
-     * 
-     *    e.g.
-     *     > 4
-     *     > 2
-     *     > 2
-     *     > 3
-     *     > 3
-     *     2
-     * Hint: Use a loop to get input. Use another 2 loops to find the mode
-     */
-     
-    Scanner in = new Scanner(System.in);
-    System.out.println("Enter a number: ");
-    int n=in.nextInt();
-    int [] a=new int[n];
-    int [] b=new int[n];
-    System.out.println("Enter " +n+ " elements");
-    double sum=0;
-    for(int i=0;i<n;i++)
-    {
-      a[i]=in.nextInt();
-    }
-    {
-      for(int i=0;i<n;i++)
-      { c=1;
-       if(a[i]==-1)
-         b[i]=0;
-       else
-       {
-         for(int j=i+1;j<n;j++)
-         }
-       if(a[i]==a[j])
-       {
-         c++;
-         a[j]=-1;
-       }
-      }
-      b[i]=c;
-    }
-  }
-  int m=b[0];
-  for(int i=1;i<n;i++)
-  {
-    if(b[i]>=m)
-      m=b[i];
-  }
+   public static void main(String args[]) 
+    { 
+	
+	Scanner sc=new Scanner(System.in);
+	System.out.println("enter a number"); 
+	int n=sc.nextInt(),c;
+	int [] a=new int[n];
+	int [] b=new int[n];
+	System.out.println("enter "+n+" elements");
+	double sum=0;
+	for(int i=0;i<n;i++) 
+	{
+		a[i]=sc.nextInt();
+		
+	}
+	modeCal(n,a,b);
+   }
+	
+static void modeCal(int n,int a[],int b[])
+   {
+	int m=0,c;	
+	for(int i=0;i<n;i++) 
+	{   c=1;
+          if(a[i]==-1)
+           b[i]=0;
+           else
+           {
+             for(int j=i+1;j<n;j++) 
+	     {
+		if(a[i]==a[j])
+                  {
+                      c++;
+                      a[j]=-1;
+                  }
+                   
+		
+	      }
+             b[i]=c;
+            }           
+           if(i>=1)
+	   {
+		if(i==1)
+		  m=b[0];
+	       if(b[i]>=m)
+                  m=b[i];   		 
+	    }		
+	}     
+        System.out.println("mode:"); 
+	 for(int i=0;i<n;i++) 
+	{
+	    if(b[i]==m)
+              System.out.println(a[i]);   
+		
+	}     	        
+   }
+ 
   
-  System.out.println("mode: ");
-  for(int i=0;i<n;i++)
-  {
-    if(b[i]==m)
-      System.out.println(a[i]);
-  }
-}
-    
-  }
 }
