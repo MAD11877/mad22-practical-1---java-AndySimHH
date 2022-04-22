6333ee13eeb81c17ac1b59fec89f79695f9e4ac0
AndySimHH <92966185+AndySimHH@users.noreply.github.com>
2022-04-22 21:55:54 +0800

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -16,7 +16,10 @@ public class Question2
     double height = in.nextDouble();
     
     System.out.print("Input weight in kg: ");
-    double height = in.nextDouble();
+    double weight = in.nextDouble();
+    
+    double BMI = weight / (height * height);
+    System.out.print("Body Mass Index is " + BMI + "\n");
     
   }
 }
