332c52adca98064b2029391d70415301752609cc
AndySimHH <92966185+AndySimHH@users.noreply.github.com>
2022-04-25 00:04:39 +0800

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -4,22 +4,25 @@ public class Question2
 {
   public static void main(String[] args)
   {
-    /**
+    /
      * Prompts user for height and weight, calculate and print out the BMI of the user
      * 
      * Hint: in.nextDouble() to read double
      */
-     
+
+    /scanner is a class in java. util package used for obtaining the input of 
+    the primitive types like int, double, etc. and strings./
     Scanner in = new Scanner(System.in);
-    
-    
+
+    /prompting input & setting variable/
     double height = in.nextDouble();
-    
-    
     double weight = in.nextDouble();
-    
-    double BMI = weight/(height*height);
-    System.out.print(BMI);
-    
+
+    /calculation/
+    double bmi = weight/(heightheight);
+
+    /displaying BMI/
+    System.out.println(bmi);
+
   }
 }
